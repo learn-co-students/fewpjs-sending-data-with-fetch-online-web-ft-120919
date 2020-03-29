0260dcc661a44e743828c18880efcfebcefc1ec0
github username <meliajm@gmail.com>
2020-03-28 18:19:39 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,56 @@
-// Add your code here
+document.addEventListener("DOMContentLoaded", () => {  
+    submitData("Steve", "steve@steve.com") 
+})
+
+function submitData(name, email) {
+let formData = {
+    name: name,
+    email: email
+  };
+   
+  let configObj = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(formData)
+  };
+
+  return fetch("http://localhost:3000/users", configObj)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(object) {
+    //   debugger
+    addTextToDOM(object.id);
+  })
+  .catch(function(error) {
+        // alert("Unauthorized Access");
+        // addTextToDOM(error.message);
+        addTextToDOM("Unauthorized Access");
+        });       
+      
+}
+
+function addTextToDOM(elem) {
+    const h = document.createElement('h2')
+    h.innerHTML = elem    
+    document.body.appendChild(h)
+    // debugger
+}
+
+
+
+// return fetch("http://localhost:3000/users", configObj)
+//     .then(function(response) {
+//         response.json();
+//     })
+//     .then(function(object) {
+//         debugger
+//         addTextToDOM(object.id)
+//     })
+//     .catch(function(error) {
+//         alert("Unauthorized Access");
+//         addTextToDOM(error.message);
+//       });
