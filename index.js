1184627b1509ea337212b3d927cd58f80f5917f7
Adrian082818 <mohabiradrian@gmail.com>
2020-03-31 19:23:49 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,54 @@
-// Add your code here
+// let formData = {
+//     dogName: "Byron",
+//     dogBreed: "Poodle"
+// };
+
+// let configObj = {
+//     method: "POST",
+//     headers: {
+//      "Content-Type": "application/json",
+//      "Accept": "application/json"   
+//     },
+//     body: JSON.stringify(formData)  
+// };
+
+// fetch('http://localhost:3000/dogs', configObj)
+//  .then(function(repsonse) {
+//     return Response.json();
+// })
+// .then(function(object) {
+//     console.log(object);
+// });
+
+
+function submitData(name, email) {
+    
+    let data = {
+        name: name,
+        email: email
+    };
+    
+    let configObj = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(data)
+    };
+
+    return fetch('http://localhost:3000/users', configObj)
+    .then(function(response) {
+        return response.json();
+})
+.then(function(object) {
+    // console.log(object);
+    document.body.innerHTML = object['id'];
+})
+.catch(function(error) {
+    alert("SOmething went wrong");
+    console.log(error.message);
+    document.body.innerHTML = error.message
+});
+// return fetch()
+}
