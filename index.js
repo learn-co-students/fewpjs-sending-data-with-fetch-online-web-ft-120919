eee1af28a73c90712eb4281e2668ced63268b5cd
Noah Scala <nwscala@gmail.com>
2020-03-31 13:58:30 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Add your code here
+function submitData(name, email) {
+    let userData = {
+        name: name,
+        email: email
+    };
+
+    let configObj = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(userData)
+    };
+
+    return fetch("http://localhost:3000/users", configObj)
+        .then(resp => resp.json())
+        .then(function(object) {
+            console.log("Object", object)
+            // document.body.innerHTML = `<p> User ID: ${object.id} </p> <br> <p> User Name: ${object.name} </p> <br> <p> User Email: ${object.email} </p>`
+            document.body.innerHTML += object.id
+        })
+        .catch(function(error) {
+            document.body.innerHTML = error.message
+        })
+}
