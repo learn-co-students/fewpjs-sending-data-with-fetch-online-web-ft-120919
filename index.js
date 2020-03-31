c0b4072ec331d4e090929794d12029055073199d
Brian Nicholls <nichol88@gmail.com>
2020-03-31 17:16:09 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Add your code here
+function submitData(name, email) {
+  const user = {
+    name: name,
+    email: email
+  }
+
+  const config = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    },
+    body: JSON.stringify(user)
+  }
+
+  return fetch('http://localhost:3000/users', config)
+    .then(response => response.json())
+    .then(function(json) {
+      document.body.innerHTML += json['id']
+    })
+    .catch(error => {
+      document.body.innerHTML += error
+    })
+}
