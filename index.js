f21bb6dd470a9e79db68d0a91c65fe1601d6f8b8
anupama <anupamagadde@yahoo.com>
2020-04-02 05:15:07 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Add your code here
+function submitData() {
+    let formData = {
+        name: "name",
+        email: "email"
+      };
+
+      let configObj = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify(formData)
+      };
+
+      fetch("http://localhost:3000/users", configObj)
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(object) {
+        console.log(object);
+      })
+      .catch(function(error) {
+        alert("Unauthorized Access");
+        console.log(error.message);
+      });
+}
